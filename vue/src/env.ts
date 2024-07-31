121a9d2050d698590189e4303b7e7677cfaa8742
sato <sato@yahooo.com>
2024-07-31 17:26:36 +0100

    Change denom name

--- vue/src/env.ts
+++ vue/src/env.ts
@@ -1,9 +1,19 @@
 const apiURL = import.meta.env.VITE_API_COSMOS ?? "http://localhost:1317";
 const rpcURL = import.meta.env.VITE_WS_TENDERMINT ?? "http://localhost:26657";
-const prefix = import.meta.env.VITE_ADDRESS_PREFIX ?? "cosmos";
+const prefix = import.meta.env.VITE_ADDRESS_PREFIX ?? "dev";
+const coinDenom = "dev";
+const coinMinimalDenom = "udev";
+const coinDecimals = "6";
+const coinImageUrl = "https://raw.githubusercontent.com/jmnote/z-icons/master/svg/go.svg";
+const coinGeckoId = "DEV";
 
 export const env = {
   apiURL,
   rpcURL,
   prefix,
+  coinDenom,
+  coinMinimalDenom,
+  coinDecimals,
+  coinImageUrl,
+  coinGeckoId
 };
